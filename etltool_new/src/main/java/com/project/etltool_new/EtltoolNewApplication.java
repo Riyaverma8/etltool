1240cc9cfac8387c9bab65b4cfb9ac4dac97170f
Riya <vriya011@gmail.com>
2026-04-27 08:43:01 +0100

    initialized the structure

---
+++ etltool_new/src/main/java/com/project/etltool_new/EtltoolNewApplication.java
@@ -0,0 +1,13 @@
+package com.project.etltool_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtltoolNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtltoolNewApplication.class, args);
+	}
+
+}
---
+++ etltool_new/src/test/java/com/project/etltool_new/EtltoolNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.etltool_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtltoolNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
